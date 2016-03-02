fcd1bd8260bfba16f3b37851983cba8de78bfefb
mfeltz <margaret.feltz@tufts.edu>
2016-03-02 15:13:10 -0500

    gh pages

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ $(document).ready(function(){
 
 var slideOnRight = function(oldElem, newElem, dist) {
 	$(newElem).animate({'right' : dist}, 400);
-	// $(newElem).css({zIndex: 3});
+
 	$(oldElem).animate({'left': '-374px'}, 400);
 };
 
 var slideOffRight = function(oldElem, newElem, dist) {
 	$(newElem).animate({'left' : dist}, 400);
 	$(oldElem).animate({'right': '-374px'}, 400);
+};
+
+var slideToTop = function(oldElem, newElem, dist) {
+	$(newElem).animate({'top' : dist}, 400);
+	// $(oldElem).animate({'top': '626px'}, 400);
 };
 
 $('#cart-icon').click(function(){
@@ -19,8 +24,12 @@ $('#cart-icon').click(function(){
 $('#back-arrow').click(function() {
 	var oldElem = $(this).parent();
 	slideOffRight(oldElem, '#home-screen', '0px');
-})
+});
 
+$('#checkout-btn').click(function() {
+	var oldElem = $(this).parent();
+	slideToTop(oldElem, '#checkout-screen', '0px');
+});
 
 
 });
