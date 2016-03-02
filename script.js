46c85d77d29f64c6b6329c2712ba6f4e6264f548
mfeltz <margaret.feltz@tufts.edu>
2016-03-02 14:10:29 -0500

    to cart works

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
 
-var fromLeft = function(newElem, dist) {
-	console.log("clicked");
-	$(newElem).animate({'right' : dist},400);
+var slideOnRight = function(oldElem, newElem, dist) {
+	$(newElem).animate({'right' : dist}, 400);
+	// $(newElem).css({zIndex: 3});
+	$(oldElem).animate({'left': '-374px'}, 400);
+};
+
+var slideOffRight = function(oldElem, newElem, dist) {
+	$(newElem).animate({'left' : dist}, 400);
+	$(oldElem).animate({'right': '-374px'}, 400);
 };
 
 $('#cart-icon').click(function(){
-	fromLeft("#cart-screen", '0px');
-
+	var oldElem = $(this).parent();
+	slideOnRight(oldElem, '#cart-screen', '0px');
 });
+
+$('#back-arrow').click(function() {
+	var oldElem = $(this).parent();
+	slideOffRight(oldElem, '#home-screen', '0px');
+})
+
 
 
 });
