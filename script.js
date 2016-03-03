7b103d97be7f0376f88cebbde5562b9136914f3f
mfeltz <margaret.feltz@tufts.edu>
2016-03-03 17:06:52 -0500

    missing things added

--- script.js
+++ script.js
@@ -2,114 +2,137 @@ $(document).ready(function(){
 
 	var cartZIndex = 3;
 	var price = 8.39;
+	var size_text;
+	var topping1_text;
 
-	var rightSlide = function(oldElem, newElem, dist) {
+	var rightSlide = function(newElem, dist) {
 		$(newElem).animate({'left' : dist}, 400);
 	};
 
-	var leftSlide = function(oldElem, newElem, dist) {
+	var leftSlide = function(newElem, dist) {
 		$(newElem).animate({'right' : dist}, 400);
 	};
 
-	var topSlide = function(oldElem, newElem, dist) {
+	var topSlide = function(newElem, dist) {
 		$(newElem).animate({'top' : dist}, 400);
 	};
 
+	topSlide('#home-screen', '0px');
+
 	//open/close cart from home
 	$('#home-screen .cart-icon').click(function(){
-		var oldElem = $(this).parent();
-		rightSlide(oldElem, '#cart-screen', '0px');
+		rightSlide('#cart-screen', '0px');
 	});
 
 	$('#cart-screen .back-arrow').click(function() {
-		var oldElem = $(this).parent();
-		rightSlide(oldElem, '#cart-screen', '375px');
+		rightSlide('#cart-screen', '375px');
 	});
 
 	//open/close checkout from cart
 	$('#checkout-btn').click(function() {
-		var oldElem = $(this).parent();
-		topSlide(oldElem, '#checkout-screen', '0px');
+		console.log($('#cart-screen').css('zIndex'));
+		$('#checkout-screen').css({zIndex: $('#cart-screen').css('zIndex')});
+		topSlide('#checkout-screen', '0px');
 	});
 
 	$('#close-icon').click(function() {
-		var oldElem = $(this).parent();
-		topSlide(oldElem, '#checkout-screen', '626px');
+		topSlide('#checkout-screen', '626px');
 	});
+
+	//open home from cart MAY NOT WORK. not needed.
+	// $('#cont-feast').click(function() {
+	// 	console.log($('#cart-screen').css('zIndex'));
+	// 	var cartZ = parseInt($('#cart-screen').css('zIndex'));
+	// 	cartZ++;
+	// 	$('#home-screen').css({zIndex: 10});
+	// 	// topSlide('#home-screen', '0px');
+	// });
 
 	//open/close menu from home
 	$('#menu-icon').click(function(){
-		var oldElem = $(this).parent();
-		leftSlide(oldElem, '#menu-screen', '0px');
+		leftSlide('#menu-screen', '0px');
 	});
 
 	$('#menu-icon-close').click(function() {
-		var oldElem = $(this).parent();
-		leftSlide(oldElem, '#menu-screen', '375px');
+		leftSlide('#menu-screen', '375px');
 	});
 
 	//open/close view all from menu
 	$('#view-all').click(function(){
-		var oldElem = $(this).parent();
-		rightSlide(oldElem, '#view-all-screen', '0px');
+		rightSlide('#view-all-screen', '0px');
 	});
 
 	$('#view-all-screen .back-arrow').click(function() {
-		var oldElem = $(this).parent();
-		rightSlide(oldElem, '#view-all-screen', '375px');
+		rightSlide('#view-all-screen', '375px');
 	});	
 
 	//open/close cart from view all
 	$('#view-all-screen .cart-icon').click(function() {
-		var oldElem = $(this).parent();
 		$('#cart-screen').css({zIndex: 4});
-		rightSlide(oldElem, '#cart-screen', '0px');
+		rightSlide('#cart-screen', '0px');
 	});	
 
 	//open/close orangutan
 	$('.orangutan-link').click(function() {
-		var oldElem = $(this).parent();
-		rightSlide(oldElem, '#orangutan-screen', '0px');
+		rightSlide('#orangutan-screen', '0px');
 	});
 
 	$('#orangutan-screen .back-arrow').click(function() {
-		var oldElem = $(this).parent();
-		rightSlide(oldElem, '#orangutan-screen', '374px');
+		rightSlide('#orangutan-screen', '374px');
 	});
 
 	//open/close cart from orangutan
 	$('#orangutan-screen .cart-icon').click(function() {
-		var oldElem = $(this).parent();
 		$('#cart-screen').css({zIndex: 6});
-		rightSlide(oldElem, '#cart-screen', '0px');
+		rightSlide('#cart-screen', '0px');
 	});	
 
 	//open/close size selection
 	$('#select-size').click(function() {
-		var oldElem = $(this).parent();
-		rightSlide(oldElem, '#size-screen', '0px');
+		rightSlide('#size-screen', '0px');
 	});	
 
 	$('#size-screen .back-arrow').click(function() {
-		var oldElem = $(this).parent();
-		rightSlide(oldElem, '#size-screen', '374px');
+		rightSlide('#size-screen', '374px');
 	});
 
 	//open/close topping selection
 	$('#select-top-1').click(function() {
-		var oldElem = $(this).parent();
-		rightSlide(oldElem, '#topping-screen', '0px');
+		rightSlide('#topping-screen', '0px');
 	});	
 
 	$('#topping-screen .back-arrow').click(function() {
-		var oldElem = $(this).parent();
-		rightSlide(oldElem, '#topping-screen', '374px');
+		rightSlide('#topping-screen', '374px');
 	});
 
+	//open/close address screen from post checkout
+	$('#select-address').click(function() {
+		rightSlide('#address-screen', '0');
+	});
+
+	$('#address-screen .back-arrow').click(function() {
+		rightSlide('#address-screen', '374px');
+	});
+
+	//open/close payment screen from post checkout
+	$('#select-payment').click(function() {
+		rightSlide('#payment-screen', '0');
+	});
+
+	$('#payment-screen .back-arrow').click(function() {
+		rightSlide('#payment-screen', '374px');
+	});
+
+	$('#order-btn').click(function() {
+		topSlide('#processing-screen', '0px').delay(1500).topSlide('#confirm-screen', '0px');
+	});
+
+	//ADD TO CART
 	$('#add-to-cart').click(function() {
 		if($('#orangutan-size-span').text() != 'Select Size') {
 			$('#added').show(0).delay(1000).fadeOut(1000);
 		}
+		$('.place-holder').show();
 	});
 
 	// SELECTING
@@ -117,14 +140,14 @@ $(document).ready(function(){
 	$('#size-screen .op').click(function() {
 		$(this).siblings().css({fontWeight: 'normal'});
 		$(this).css({fontWeight: 'bold'});
-		var text = $(this).text();
-		$('#orangutan-size-span').html(text);
+		size_text = $(this).text();
+		$('.size-text').html(size_text);
 
-		if(text != 'Small') {
-			$('#price').html(price += 0.75);
+		if(size_text != 'Small') {
+			$('.price-text').html(price += 0.75);
 		} else {
 			if(price > 8.39) {
-				$('#price').html(price -= 0.75);
+				$('.price-text').html(price -= 0.75);
 			}
 		}
 	});
@@ -133,21 +156,22 @@ $(document).ready(function(){
 	$('#topping-screen .top-op').click(function() {
 		$(this).siblings().css({fontWeight: 'normal'});
 		$(this).css({fontWeight: 'bold'});
-		var text = $(this).text();
-		if (text == "None") {
-			text = 'Topping #1  (+ $0.75)';
-			if(price > 9.39) {
-				$('#price').html(price -= 0.75);
-			}
-		} else {
-			if(price != 9.89) {
-				$('#price').html(price += 0.75);
-			}
-		}
-		$('#orangutan-top-1').html(text);
-	});
+		topping1_text = $(this).text();
+		
+		if (topping1_text == "None") {
+			topping1_text = 'Topping #1  (+ $0.75)';
 
-//work on topping price logic
+			if((size_text == 'Small' && price == 9.14) ||
+				(size_text == 'Large (+ $0.75)' && price == 9.89)) {
+				$('.price-text').html(price -= 0.75);
+			}
+
+		} else if((size_text == 'Small' && price == 8.39) ||
+				(size_text == 'Large (+ $0.75)' && price == 9.14)) {
+				$('.price-text').html(price += 0.75);
+		}
+		$('.topping-text').html(topping1_text);
+	});
 
 });
 
